29195edb1125b771ee0449c2cd85a7c00fa18411
Linh Pham <dev@wwdt.me>
2021-03-10 18:59:11 -0800

    Bump version

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from reports.panel import aggregate_scores, gender_mix
 from reports.show import bluff_count as bluff, scores as show_scores
 
 #region Global Constants
-APP_VERSION = "1.8.0"
+APP_VERSION = "1.8.0.1"
 
 #endregion
 
