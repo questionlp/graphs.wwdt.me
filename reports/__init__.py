c047f9f578a08e7f9e31c72ac64bd6406bbc95f3
Linh Pham <dev@wwdt.me>
2021-07-05 08:40:38 -0700

    Adding Show Counts by Year Graph

--- reports/__init__.py
+++ reports/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2018-2020 Linh Pham
+# Copyright (c) 2018-2021 Linh Pham
 # graphs.wwdt.me is relased under the terms of the Apache License 2.0
 """Explicitly listing all reporting modules"""
 
 from reports.panel import aggregate_scores, gender_mix
-from reports.show import bluff_count, scores
+from reports.show import bluff_count, scores, show_counts
 
 __all__ = [
     "panel",
