9d49069ee435e6635b1e58cc9e5b366e935a081f
Linh Pham <dev@wwdt.me>
2021-08-13 21:07:25 -0700

    Add new Counts by Day of Month set of charts

---
+++ reports/show/dates.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018-2021 Linh Pham
+# reports.wwdt.me is relased under the terms of the Apache License 2.0
+"""WWDTM Show Dates Retrieval Functions"""
+
+from collections import OrderedDict
+from typing import Dict
+import mysql.connector
+
+#region Utility Functions
+def build_days_of_month_dict(month: int) -> Dict:
+    """Returns an OrderedDict containing a key for each day for a given
+    month, each containing an OrderedDict used to store counts by show
+    type"""
+
+    # Validate that the month number is valid
+    if not month in range(1, 13):
+        return None
+
+    if month == 2:
+        days_in_month = 29
+    elif month in [1, 3, 5, 7, 8, 10, 12]:
+        days_in_month = 31
+    else:
+        days_in_month = 30
+
+    month = OrderedDict()
+    for day in range(1, days_in_month + 1):
+        show_info = OrderedDict()
+        show_info["regular"] = 0
+        show_info["best_of"] = 0
+        show_info["repeat"] = 0
+        show_info["best_of_repeat"] = 0
+        month[day] = show_info
+
+    return month
+
+#endregion
+
+#region Retrieval Functions
+def retrieve_show_counts_by_month_day(month: int,
+                                      database_connection: mysql.connector.connect
+                                     ) -> Dict:
+    """Retrieves an OrderedDict containing a count of regular shows,
+    Best Of shows, repeat shows and repeat Best Of shows for each day
+    of the requested month"""
+
+    # Validate that the month number is valid
+    if not month in range(1, 13):
+        return None
+
+    show_month = build_days_of_month_dict(month)
+    if not show_month:
+        return None
+
+    cursor = database_connection.cursor(dictionary=True)
+    query = ("SELECT DAY(showdate) AS day, bestof, repeatshowid FROM ww_shows "
+             "WHERE MONTH(showdate) = %s "
+             "ORDER BY DAY(showdate) ASC;")
+    cursor.execute(query, (month, ))
+    results = cursor.fetchall()
+    cursor.close()
+
+    if not results:
+        return None
+
+    for row in results:
+        day = row["day"]
+        best_of = bool(row["bestof"])
+        repeat_show = bool(row["repeatshowid"])
+
+        if not best_of and not repeat_show:
+            show_month[day]["regular"] += 1
+        elif best_of and not repeat_show:
+            show_month[day]["best_of"] += 1
+        elif not best_of and repeat_show:
+            show_month[day]["repeat"] += 1
+        elif best_of and repeat_show:
+            show_month[day]["best_of_repeat"] += 1
+
+    return show_month
+
+#endregion
