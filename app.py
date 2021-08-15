7aa0e507a7cd4ff4032f5d94a195dee559027ed5
Linh Pham <dev@wwdt.me>
2021-08-15 12:32:15 -0700

    Add repo_url to globals

--- app.py
+++ app.py
@@ -547,6 +547,7 @@ app.jinja_env.globals["blog_url"] = config["settings"]["blog_url"]
 app.jinja_env.globals["reports_url"] = config["settings"]["reports_url"]
 app.jinja_env.globals["site_url"] = config["settings"]["site_url"]
 app.jinja_env.globals["stats_url"] = config["settings"]["stats_url"]
+app.jinja_env.globals["repo_url"] = config["settings"]["repo_url"]
 
 database_connection = mysql.connector.connect(**config["database"])
 database_connection.autocommit = True
