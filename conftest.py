c141bd9605130a127fca3b6321f05f62f14b00a2
Linh Pham <question+github@closedsrc.org>
2024-07-08 22:01:32 -0700

    Add docstring and properly name inner client name

--- conftest.py
+++ conftest.py
@@ -5,12 +5,14 @@
 # vim: set noai syntax=python ts=4 sw=4:
 """pytest conftest.py File."""
 import pytest
+from flask import Flask
 
 from app import create_app
 
 
 @pytest.fixture
 def client():
-    app = create_app()
-    with app.test_client() as client:
-        yield client
+    """Pytest Client Fixture."""
+    app: Flask = create_app()
+    with app.test_client() as _client:
+        yield _client
