8ac3cd560f2dc64760f36c914f6eb3dfe6655f79
Linh Pham <question+github@closedsrc.org>
2025-12-25 13:12:33 -0800

    Updated module docstrings for app/reports/**/__init__.py

--- app/reports/location/__init__.py
+++ app/reports/location/__init__.py
@@ -3,4 +3,4 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # vim: set noai syntax=python ts=4 sw=4:
-"""Locations Module for Wait Wait Graphs Site."""
+"""Locations Reports Module for Wait Wait Graphs Site."""
---
+++ app/reports/scorekeepers/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2018-2025 Linh Pham
+# graphs.wwdt.me is released under the terms of the Apache License 2.0
+# SPDX-License-Identifier: Apache-2.0
+#
+# vim: set noai syntax=python ts=4 sw=4:
+"""Scorekeepers Reports Module for Wait Wait Graphs Site."""
