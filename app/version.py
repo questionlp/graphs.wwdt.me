7394efa96db4cf1c238ebbdc13f9dcf4e73f83b1
Linh Pham <question+github@closedsrc.org>
2026-01-29 19:21:08 -0800

    Add higher contrast dark theme, update AI bots in robots.txt, update dependencies

--- app/version.py
+++ app/version.py
@@ -1,8 +1,8 @@
-# Copyright (c) 2018-2025 Linh Pham
+# Copyright (c) 2018-2026 Linh Pham
 # graphs.wwdt.me is released under the terms of the Apache License 2.0
 # SPDX-License-Identifier: Apache-2.0
 #
 # vim: set noai syntax=python ts=4 sw=4:
 """Application Version for Wait Wait Graphs Site."""
 
-APP_VERSION = "3.12.0-alpha"
+APP_VERSION = "3.12.0"
